d1009feefb1095793f8b5d539005e16c159242e6
simoneclr <simone.clclr@gmail.com>
2022-06-21 16:25:05 +0200

    Add top menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
+import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="h-screen">
+    <div className="h-screen flex">
 
       <Sidebar/>
       
-      <main>
-
+      <main className='grow'>
+        <Navbar/>
       </main>
     </div>
   );
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Displays the navigation bar at the top of the app
+function Navbar() {
+	return (
+		<nav className="flex items-center px-8 py-4 gap-8 bg-slate-400">
+			<h1 className="text-3xl">
+				Board Title
+			</h1>			
+		</nav>
+	)
+}
+
+export default Navbar
