4dc742924668c2a838986cfd5e00af97580703c0
simoneclr <simone.clclr@gmail.com>
2022-06-28 16:52:48 +0200

    Fix bug with modal click handling

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,10 +7,17 @@ interface ModalProps {
 }
 
 function Modal({isOpen, close, children}: ModalProps) {
+
+	const handleModalClick = (e: SyntheticEvent) => {
+		if (e.target === e.currentTarget) {
+			close(e)
+		}
+	}
+
 	return (
 		isOpen ?
 
-		<div onClick={e => close(e)}
+		<div onClick={handleModalClick}
 			className="fixed inset-0 flex items-center justify-center bg-black/75">
 			<div className="flex flex-col gap-4 w-1/3 p-8 bg-slate-50 rounded">
 				{children}
